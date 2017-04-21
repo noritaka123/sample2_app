c34b093bd7b8176606d6b57fa626a3e6a6f7c921
funayamanoritaka <f.noritaka123@gmail.com>
2017-04-21 12:00:40 +0900

    Use SSL2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,7 @@ end
   end
 
   def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
 
 end
